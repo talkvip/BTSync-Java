9befee31b59c76ae4f893dfc4957375ed6100fbd
omeed <omeed@omeed-700T1C>
2014-07-10 21:09:46 -0500

    Untracking original Maven based project

--- BTSync-Java/src/ms/safi/btsync/dto/package-info.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * Domain classes used to produce and consume the JSON for BitTorrent Sync RESTful services.
- * 
- * @author Omeed Safi
- *
- */
-package ms.safi.btsync.dto;
--- BTSync-Java/src/ms/safi/btsync/util/package-info.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * Utility classes used by other packages.
- * 
- * @author Omeed
- *
- */
-package ms.safi.btsync.util;
